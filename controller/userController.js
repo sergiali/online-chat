a5b5f3edf793c8fc23a753055a28f867bd22f002
alib4 <alibcr80547@gmail.com>
2022-09-12 10:07:13 +0430

    correct hashing password use from pre middleware & send email after success register & create these methods ->(forget password , handle forget password , reset password , handle reset password)

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,8 @@
-const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const User = require('../model/user');
+const { sendEmail } = require('../utils/mailer');
 
 exports.login = (req,res) => {
     res.render("login",{
@@ -61,12 +62,26 @@ exports.createUser = async (req,res) => {
             });
         }
 
-        const hash = await bcrypt.hash(password,10);
+        // const hash = await bcrypt.hash(password,10);
+        // await User.create({
+        //     fullname,
+        //     email,
+        //     password:hash,
+        // });
         await User.create({
             fullname,
             email,
-            password:hash,
+            password,
         });
+
+        //? Send Wellcom Email
+        sendEmail(
+            email,
+            fullname,
+            " اپلیکیشن چت ",
+            " خوش آمدی و اینجا میتونی با دوستات چت کنی و لذت ببری "
+            );
+
         req.flash("success-msg","ثبت نام با موفقیت انجام شد");
         res.redirect("/users/login");
     } catch (err) {
@@ -84,4 +99,96 @@ exports.createUser = async (req,res) => {
             errors,
         });
     };
+};
+
+exports.forgetPassword = async (req,res) => {
+    res.render("forgetPass",{
+        pageTitle: " فراموشی رمز عبور ",
+        path: "/login",
+        message: req.flash("success-msg"),
+        error: req.flash("error"),
+    });
+};
+
+exports.handleForgetPassword = async (req,res) => {
+    const {email} = req.body;
+    const user = await User.findOne({email: email});
+    if(!user){
+        req.flash("error","کاربری با این ایمیل در پایگاه داده ثبت نیست");
+
+        return res.render("forgetPass",{
+            pageTitle: " فراموشی رمز عبور ",
+            path: "/login",
+            message: req.flash("success-msg"),
+            error: req.flash("error"),
+        });
+    };
+
+    const token = jwt.sign({userId: user._id},process.env.JWT_SECRET,{expiresIn:"1h"});
+    const resetLink = `http://localhost:3000/users/reset-password/${token}`;
+
+    sendEmail(
+        user.email,
+        user.fullname,
+        "فراموشی رمز عبور",
+        `
+        جهت تغییر رمز عبور روی لینک زیر کلیک کنید
+        <a href=" ${resetLink}">  لینک تغییر رمز عبور</a>
+    `);
+    req.flash("success-msg","ایمیل حاوی لینک با موفقیت ارسال شد");
+
+    res.render("forgetPass",{
+        pageTitle: " فراموشی رمز عبور ",
+        path: "/login",
+        message: req.flash("success-msg"),
+        error: req.flash("error"),
+    });
+};
+
+exports.resetPassword = (req,res) => {
+    const token = req.params.token;
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token,process.env.JWT_SECRET);
+        console.log(decodedToken);
+    } catch (err) {
+        console.log(err);
+        if(!decodedToken){
+            return res.redirect("/404");
+        };
+    };
+        res.render("resetPass",{
+            pageTitle:" تغییر رمز عبور ",
+            path: "/login",
+            message: req.flash("success-msg"),
+            error: req.flash("error"),
+            userId:decodedToken.userId,
+        });
+};
+
+exports.handleResetPassword = async (req,res) => {
+    const {password,confirmPassword} = req.body;
+    if(password !== confirmPassword)
+    {
+        req.flash("error","کلمه های عبور یکسان نیستند");
+
+        return res.render("resetPass",{
+            pageTitle:" تغییر رمز عبور ",
+            path: "/login",
+            message: req.flash("success-msg"),
+            error: req.flash("error"),
+            userId:req.params.id,
+        });
+    };
+
+    const user = await User.findOne({_id: req.params.id});
+    if(!user){
+        return res.redirect("/404")
+    }
+    user.password = password;
+    await user.save();
+
+    req.flash("success-msg","کلمه عبور شما با موفقیت تغییر یافت");
+    res.redirect("/users/login");
 };
