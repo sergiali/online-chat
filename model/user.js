66e89b080420c180ee41bd292c0687a933f50066
alib4 <alibcr80547@gmail.com>
2022-09-12 10:07:53 +0430

    create pre middleware for hashing user password before save in database

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const { schema } = require('./secure/userValidation');
 
 const userSchema = new mongoose.Schema({
@@ -29,6 +29,21 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.userValidation = function (body){
     return schema.validate(body, {abortEarly: false});
 };
+
+userSchema.pre("save",function(next){
+    let user = this;
+    
+    if(!user.isModified("password") )
+    return next();
+
+    bcrypt.hash(user.password,10,(err,hash) => {
+        if(err){
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
 
 const User = mongoose.model("User",userSchema);
 
